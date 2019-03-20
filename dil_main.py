12af3bfec75dfed163e345e2b5ef737ed9d6ea80
Nick Silva <silva.d.nicholas@gmail.com>
2019-03-20 10:22:29 -0500

    added documentation

--- dil_main.py
+++ dil_main.py
@@ -1,14 +1,14 @@
-import dilema
+import dilemma
 import random
 
-firm1 = dilema.Firm()
-firm2 = dilema.Firm()
+firm1 = dilemma.Firm()
+firm2 = dilemma.Firm()
 firm1.create()
 firm2.create()
 
 epsilon = 1 #rate at which it randomly explores
 
-for i in range(1000): #number of iterations at each price set
+for i in range(1000): #number of iterations
     if(epsilon > random.random()): #should we explore or not
         action1 = random.randint(0, 2)
         action2 = random.randint(0, 2)
@@ -19,16 +19,17 @@ for i in range(1000): #number of iterations at each price set
         action2 = firm2.best_action()
         firm1.run(action1, action2)
         firm2.run(action2, action1)
-    epsilon -= .001
+    epsilon -= .001 #decreasing the exploration rate so that it learns using previous q values
 
 def nash_equilibrium(firm1n, firm2n):
+    #attempts to find the nash equilbrium for the two tables so that it can be compared to the qTable
     output = []
     for i in range (len(firm1n)):
         if firm1n[i] == firm2n[i]:
             output.append((firm1n[i], firm2n[i]))
     return output
 
-
+#display the results
 print(firm1.best_action(), "\n", firm1.printp())
 print(firm1.getq())
 print(firm2.find_nash())
--- dilemma.py
+++ dilemma.py
@@ -3,34 +3,43 @@ import random
 class Firm:
 
   q_table = []
+  #holds benefit of making a decision
   p_table = [[]]
+  #holds the direct payoff for any action (column) given the other firms action (row)
 
   learning_rate = .8 #rate at which it values new data
   discount_rate = .2 #rate at which it values future benefit
 
   def create(self):
     self.p_table = [[random.randint(0, 10) for x in range(3)] for y in range(3)]
+    #randomly creates a payoff matrix
     self.q_table = [0,0,0]
     #holds the long term benefit of any given decision
 
   def run(self, my_action, their_action):
+    #runs one trial given two chosen actions
+
     self.q_table[my_action] = self.q_table[my_action] + self.learning_rate *(self.p_table[their_action][my_action] - self.discount_rate * self.max_col(self.p_table[their_action]))
+    #Changes the current q table value given the payoff of the current situation
 
     if(self.learning_rate > .005):
+        #decreases learning rate and increases discount rate
         self.learning_rate -= .002
         self.discount_rate += .002
-    #self.q_table[x] = self.q_table[x] + self.learning_rate * (x * b1 + self.discount_rate*(self.q_table[2][self.max_col(self.q_table[2][:(self.wealth + 1)])] - self.q_table[1][x]))
 
   def best_action(self):
+    #returns the action with the highest value in the q table
     return self.max_col(self.q_table)
 
   def printp(self):
+    #prints the 3x3 array
     return str(self.p_table[0]) + "\n" + str(self.p_table[1]) + "\n" + str(self.p_table[2])
 
   def getq(self):
-      return self.q_table
+    return self.q_table
 
   def max_col(self, table):
+    #given a row this returns the index of the largest value in that row
     max = table[0]
     max_col_index = 0
     for i in range(1, len(table)):
@@ -41,7 +50,8 @@ class Firm:
     return max_col_index
 
   def find_nash(self):
-        nash_table = []
-        for i in range(len(self.p_table[0])):
-            nash_table.append(self.max_col(self.p_table[i]))
+      #returns the largest number in each row i.e. the best decision given the other firm's action
+      nash_table = []
+      for i in range(len(self.p_table[0])):
+        nash_table.append(self.max_col(self.p_table[i]))
         return nash_table
